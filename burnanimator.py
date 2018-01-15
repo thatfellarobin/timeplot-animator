fc9a9c0ac512b6e214174ad6c37fd3e990b5dd28
Robin Liu <Robin.Liu.831@gmail.com>
2018-01-14 21:55:47 -0500

    cleaned up script slightly and added empty frames folder

--- burnanimator.py
+++ burnanimator.py
@@ -1,5 +1,4 @@
 import numpy as np
-# import pandas as pd
 import matplotlib.pyplot as plt
 
 data = np.loadtxt('data.csv',delimiter = ',',skiprows = 1,usecols = [0,5])
@@ -10,23 +9,21 @@ t_start = 382 # seconds
 t_end = 410 # seconds
 framerate = 30 # frames per second
 
+# Preparing cropped list of x values
 t_delta = data[1,0] - data[0,0]
 t_domain = t_end - t_start
 xs = np.arange(0,t_domain,t_delta)
 
+# Preparing cropped list of y values
 t_start_index = int(round((t_start - data[0,0]) / t_delta))
 total_num_indices = int(round(((t_end - t_start)/t_delta))) + 1
 t_end_index = t_start_index + total_num_indices
-
 ys = data[t_start_index:t_end_index,1]
-# print(xs)
-# print(ys)
 
 num_frames = t_domain * framerate
 frame_tstep = 1. / framerate
 
 fig,ax = plt.subplots()
-
 
 for frame in range(num_frames): # replace range with num_frames
     ax.cla()
@@ -46,15 +43,7 @@ for frame in range(num_frames): # replace range with num_frames
     frameindex = int(round(frametime / t_delta))
     ax.plot(xs[0:frameindex],ys[0:frameindex], 'r', linewidth = 3)
     
-    # Line properties
-
-
     fig.savefig('frames/frame_' + str(frame) + '.png')
 
     print('saved frame',frame)
-
-
-
-
-
 
