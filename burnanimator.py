75e10b890c20e73ca7dd35eed9fa4f48cfcd8aa4
Robin Liu <Robin.Liu.831@gmail.com>
2018-12-18 18:51:41 -0500

    implemented interpolation

--- burnanimator.py
+++ burnanimator.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy import interpolate
 import matplotlib.pyplot as plt
 import os
 
@@ -6,23 +7,37 @@ data = np.loadtxt('data.csv',delimiter = ',',skiprows = 1,usecols = [0,1])
 
 # User-inputted parameters
 t_start = 2505 # In the same time units as your data
-t_end = 2506.5 # In the same time units as your data
-anim_scale = 1 # Scaling for animation speed, ie seconds of animation per data-time unit.
+t_end = 2515 # In the same time units as your data
+animation_scale = 1 # Scaling for animation speed, ie seconds of animation per data-time unit.
 framerate = 30 # frames per second of animation
+run_interpolate = False # If your data sample rate is low, you can use this to interpolate intermediate values for smoother animation
+run_smooth = False # If your data is noisy, you can use this to make the data smoother
 
 # Preparing cropped list of x values
 t_delta = data[1,0] - data[0,0]
 t_domain = t_end - t_start
-xs = np.arange(0,t_domain,t_delta)
+xs = np.linspace(0,t_domain,num=t_domain/t_delta)
 
 # Preparing cropped list of y values
 t_start_index = int(round((t_start - data[0,0]) / t_delta))
-total_num_indices = int(round(t_domain/t_delta)) + 1
-t_end_index = t_start_index + total_num_indices
+t_end_index = t_start_index + len(xs)
 ys = data[t_start_index:t_end_index,1]
 
-num_frames = int(t_domain * framerate * anim_scale)
+num_frames = int(t_domain * framerate * animation_scale)
 frame_tstep = 1. / framerate # How much animation-time passes per frame
+
+if run_interpolate:
+    f = interpolate.interp1d(xs,ys,kind='quadratic')
+    new_xs = np.linspace(0,t_domain,num=num_frames)
+    t_delta = new_xs[1] - new_xs[0]
+    new_ys = f(new_xs)
+
+if run_smooth:
+    pass
+
+if run_interpolate or run_smooth:
+    xs = new_xs
+    ys = new_ys
 
 fig,ax = plt.subplots()
 
@@ -48,8 +63,8 @@ for frame in range(num_frames): # replace range with num_frames when not testing
     ax.grid()
 
     # Plotting
-    frametime = frame_tstep * frame
-    frameindex = int(round(frametime / (anim_scale * t_delta)))
+    animation_time = frame_tstep * frame
+    frameindex = int(round(animation_time / (animation_scale * t_delta)))
     ax.plot(xs[0:frameindex],ys[0:frameindex], 'r', linewidth = 3)
 
     fig.savefig('frames/frame_' + str(frame) + '.png', dpi = 200)
