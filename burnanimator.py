528fad4dd9a3424877d7cf2429b849235b0b8c43
Robin Liu <Robin.Liu.831@gmail.com>
2018-12-19 06:56:57 -0500

    removed unecessary commented out savgol block

--- burnanimator.py
+++ burnanimator.py
@@ -2,29 +2,6 @@ import numpy as np
 from scipy import interpolate, signal
 import matplotlib.pyplot as plt
 import os
-from math import factorial
-
-# def savitzky_golay(y, window_size, order, deriv=0, rate=1):
-#     try:
-#         window_size = np.abs(np.int(window_size))
-#         order = np.abs(np.int(order))
-#     except ValueError, msg:
-#         raise ValueError("window_size and order have to be of type int")
-#     if window_size % 2 != 1 or window_size < 1:
-#         raise TypeError("window_size size must be a positive odd number")
-#     if window_size < order + 2:
-#         raise TypeError("window_size is too small for the polynomials order")
-#     order_range = range(order+1)
-#     half_window = (window_size -1) // 2
-#     # precompute coefficients
-#     b = np.mat([[k**i for i in order_range] for k in range(-half_window, half_window+1)])
-#     m = np.linalg.pinv(b).A[deriv] * rate**deriv * factorial(deriv)
-#     # pad the signal at the extremes with
-#     # values taken from the signal itself
-#     firstvals = y[0] - np.abs( y[1:half_window+1][::-1] - y[0] )
-#     lastvals = y[-1] + np.abs(y[-half_window-1:-1][::-1] - y[-1])
-#     y = np.concatenate((firstvals, y, lastvals))
-#     return np.convolve( m[::-1], y, mode='valid')
 
 data = np.loadtxt('data.csv',delimiter = ',',skiprows = 1,usecols = [0,1])
 
@@ -53,7 +30,6 @@ if run_smooth:
     smoothing_window = min(4,2*len(xs)/num_frames,10)
     if smoothing_window <= 4:
         print("warning: small smoothing window")
-    # ys = savitzky_golay(ys, window=smoothing_window, order=3)
     ys = signal.savgol_filter(ys, window_length=smoothing_window, polyorder=3)
 
 if run_interpolate:
