8113659c46294a38f400976f5e51e62528767ac8
Robin Liu <Robin.Liu.831@gmail.com>
2018-12-30 09:17:16 -0700

    Added checkbuttons and some tk variables

--- guitest/plot_anim_gui_test.py
+++ guitest/plot_anim_gui_test.py
@@ -1,6 +1,17 @@
 import tkinter as tk
 
 root = tk.Tk()
+
+t_start = tk.DoubleVar() # In the same time units as your data
+t_end = tk.DoubleVar() # In the same time units as your data
+animation_scale = tk.DoubleVar() # Scaling for animation speed, ie seconds of animation per data-time unit.
+framerate = tk.DoubleVar() # frames per second of animation
+run_smooth = tk.IntVar() # If your data is noisy, you can use this to make the data smoother
+run_interpolate = tk.IntVar() # If your data sample rate is low, you can use this to interpolate intermediate values for smoother animation
+graph_title = tk.StringVar()
+graph_ylabel = tk.StringVar()
+graph_xlabel = tk.StringVar()
+
 root.title('Timeplot Animator')
 
 param_frame = tk.Frame(root)
@@ -11,11 +22,15 @@ param_label.pack()
 smoothing_frame = tk.Frame(root)
 smoothing_frame.grid(row=1,column=0)
 smoothing_label = tk.Label(smoothing_frame, text='Smoothing')
+smoothing_check = tk.Checkbutton(smoothing_frame,text='Apply Smoothing',variable=run_smooth)
+smoothing_check.pack()
 smoothing_label.pack()
 
 interp_frame = tk.Frame(root)
 interp_frame.grid(row=2,column=0)
 interp_label = tk.Label(interp_frame, text='Interpolation')
+interp_check = tk.Checkbutton(interp_frame,text='Apply Interpolation',variable=run_interpolate)
+interp_check.pack()
 interp_label.pack()
 
 preview_frame = tk.Frame(root)
