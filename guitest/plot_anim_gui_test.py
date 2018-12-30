831910100b8ab7bdd84f99dd43c05b71d24e0d1c
Robin Liu <Robin.Liu.831@gmail.com>
2018-12-29 22:10:24 -0700

    messing around

--- guitest/plot_anim_gui_test.py
+++ guitest/plot_anim_gui_test.py
@@ -4,12 +4,25 @@ root = tk.Tk()
 root.title('Timeplot Animator')
 
 param_frame = tk.Frame(root)
-param_frame.pack(side = BOTTOM)
-smoothing_frame = tk.Frame(root)
-smoothing_frame.pack(side = BOTTOM)
-interp_frame = tk.Frame(root)
-interp_frame.pack(side = BOTTOM)
-preview_frame = tk.Frame(root)
-preview_frame.pack(side = BOTTOM) # Eventually have the preview frame on the right
+param_label = tk.Label(param_frame, text='General Parameters', width=50).grid(row=0,column=0)
+param_label = tk.Label(param_frame, text='Test', width=50).grid(row=1,column=0)
+# param_frame.grid(row=0,column=0)
 
-root.mainloop()
+# smoothing_frame = tk.Frame(root).grid(row=1,column=0)
+# smoothing_label = tk.Label(smoothing_frame, text='Smoothing').pack()
+# # smoothing_frame.pack()
+#
+# interp_frame = tk.Frame(root)#.grid(row=2,column=0)
+# interp_label = tk.Label(interp_frame, text='Interpolation')
+# # interp_frame.pack()
+#
+# preview_frame = tk.Frame(root)#.grid(row=0,column=1)
+# preview_label = tk.Label(preview_frame, text='Plot Preview (final frame)')
+# # preview_frame.pack() # Eventually have the preview frame on the right
+
+tk.mainloop()
+
+# TODO eventually:
+# - explanation under each section label
+# - Organize frames by grid, with proper sizing
+# - Figure out how to use field entries
