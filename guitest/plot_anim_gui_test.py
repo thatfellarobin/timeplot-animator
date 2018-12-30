fce8ee53e13841a757ea21a70ea1954fddaf6c28
Robin Liu <Robin.Liu.831@gmail.com>
2018-12-29 22:28:49 -0700

    got the basic pieces in the right places

--- guitest/plot_anim_gui_test.py
+++ guitest/plot_anim_gui_test.py
@@ -4,21 +4,24 @@ root = tk.Tk()
 root.title('Timeplot Animator')
 
 param_frame = tk.Frame(root)
-param_label = tk.Label(param_frame, text='General Parameters', width=50).grid(row=0,column=0)
-param_label = tk.Label(param_frame, text='Test', width=50).grid(row=1,column=0)
-# param_frame.grid(row=0,column=0)
+param_frame.grid(row=0,column=0)
+param_label = tk.Label(param_frame, text='General Parameters', width=40)
+param_label.pack()
 
-# smoothing_frame = tk.Frame(root).grid(row=1,column=0)
-# smoothing_label = tk.Label(smoothing_frame, text='Smoothing').pack()
-# # smoothing_frame.pack()
-#
-# interp_frame = tk.Frame(root)#.grid(row=2,column=0)
-# interp_label = tk.Label(interp_frame, text='Interpolation')
-# # interp_frame.pack()
-#
-# preview_frame = tk.Frame(root)#.grid(row=0,column=1)
-# preview_label = tk.Label(preview_frame, text='Plot Preview (final frame)')
-# # preview_frame.pack() # Eventually have the preview frame on the right
+smoothing_frame = tk.Frame(root)
+smoothing_frame.grid(row=1,column=0)
+smoothing_label = tk.Label(smoothing_frame, text='Smoothing')
+smoothing_label.pack()
+
+interp_frame = tk.Frame(root)
+interp_frame.grid(row=2,column=0)
+interp_label = tk.Label(interp_frame, text='Interpolation')
+interp_label.pack()
+
+preview_frame = tk.Frame(root)
+preview_frame.grid(row=0,column=1)
+preview_label = tk.Label(preview_frame, text='Plot Preview (final frame)')
+preview_label.pack()
 
 tk.mainloop()
 
